0cf24eb113f86c59babc48d707dd385687625a8c
Atharva Kadam <100138079+Atharva0402@users.noreply.github.com>
2024-03-30 20:41:05 +0530

    Update page.tsx
    changed the transiton from relative to not relative

--- app/(Home)/page.tsx
+++ app/(Home)/page.tsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer'
 function page() {
   return (
     <div className='min-h-screen bg-black overflow-hidden'>
-        <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
+        <div className='dark:bg-black bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2]'>
             <div className='max-w-7xl mx-auto p-5 '>
           <Navbar/>
           <HeroSection/>
